26d1a4f2ea4e05a331a8ca9eec170553c56e07a1
Berth <al.galdamezh@gmail.com>
2020-09-11 17:54:57 -0600

    Aling Sintaxis changes on download.js

--- src/routes/download.js
+++ src/routes/download.js
@@ -1,25 +1,27 @@
-const {Router} = require('express');
+const {
+    Router
+} = require('express');
 const router = Router();
 
-router.get('/offensive', (req, res)=>{
-    res.download('./src/database/offensive.json', 'TF2-Offensive_Classes.json', (err) =>{
-        if(err){
+router.get('/offensive', (req, res) => {
+    res.download('./src/database/offensive.json', 'TF2-Offensive_Classes.json', (err) => {
+        if (err) {
             return console.log('Error Searching The File.');
             res.send('Error')
         }
     });
 })
 
-router.get('/deffensive', (req, res) =>{
-    res.download('./src/database/deffensive.json', 'TF2-Deffensive_Classes.json', (err)=>{
-        if(err) return res.end();
-        
+router.get('/deffensive', (req, res) => {
+    res.download('./src/database/deffensive.json', 'TF2-Deffensive_Classes.json', (err) => {
+        if (err) return res.end();
+
     })
 })
 
-router.get('/support', (req, res)=>{
-    res.download('./src/database/support.json', 'TF2-Support_Classes.json', (err)=>{
-        if(err) return res.send('Err')
+router.get('/support', (req, res) => {
+    res.download('./src/database/support.json', 'TF2-Support_Classes.json', (err) => {
+        if (err) return res.send('Err')
     })
 })
 
