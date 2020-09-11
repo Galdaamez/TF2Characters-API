12f586c5656f4efd92afe829586eb5a1517f415d
Berth <al.galdamezh@gmail.com>
2020-09-11 13:51:16 -0600

    Download route created

---
+++ src/routes/download.js
@@ -0,0 +1,27 @@
+const {Router} = require('express');
+const { route } = require('./deffensive');
+const router = Router();
+
+router.get('/offensive', (req, res)=>{
+    res.download('./src/database/offensive.json', 'TF2-Offensive_Classes.json', (err) =>{
+        if(err){
+            return console.log('Error Searching The File.');
+            res.send('Error')
+        }
+    });
+})
+
+router.get('/deffensive', (req, res) =>{
+    res.download('./src/database/deffensive.json', 'TF2-Deffensive_Classes.json', (err)=>{
+        if(err) return res.end();
+        
+    })
+})
+
+router.get('/support', (req, res)=>{
+    res.download('./src/database/support.json', 'TF2-Support_Classes.json', (err)=>{
+        if(err) return res.send('Err')
+    })
+})
+
+module.exports = router;
